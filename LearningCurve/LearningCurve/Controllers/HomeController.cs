b842d86bce3829231531a8307e543dc0695403b5
RM Lombard <rm.awsum@gmail.com>
2017-09-26 21:24:52 +0200

    CRUD for kendo grid

--- LearningCurve/LearningCurve/Controllers/HomeController.cs
+++ LearningCurve/LearningCurve/Controllers/HomeController.cs
@@ -1,30 +1,74 @@
-﻿using System;
+﻿using LearningCurve.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearningCurve.Common;
 
 namespace LearningCurve.Controllers
 {
     public class HomeController : Controller
     {
+        private int count = 10;
         public ActionResult Index()
         {
             return View();
+        }  
+        
+        public JsonResult GetData()
+        {
+            var list = new List<GridData>();
+            for (int i = 1; i < 11; i++)
+            {
+                var item = new GridData()
+                {
+                    ID = i,
+                    ForeignKeyField = 1,
+                    TextField = string.Format("Item {0}", i.ToString())
+                };
+                list.Add(item);
+            }
+
+            IEnumerable<GridData> result = list;
+
+            return this.Jsonp(result);
+        }    
+        
+        public JsonResult Create()
+        {
+            var models = this.DeserializeObject<IEnumerable<GridData>>("models");
+            if (models != null)
+            {
+                //...Temp - illustrate save
+                count++;
+                models.FirstOrDefault().ID = count;
+
+                //...If you get more than one object, means the previous save didn't commit. 
+                //...Probably didnt change the ID
+            }
+            return this.Jsonp(models);
         }
 
-        public ActionResult About()
+        public JsonResult Update()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var models = this.DeserializeObject<IEnumerable<GridData>>("models");
+            if (models != null)
+            {
+                
+            }
+            return this.Jsonp(models);
         }
 
-        public ActionResult Contact()
+        public JsonResult Destroy()
         {
-            ViewBag.Message = "Your contact page.";
+            var models = this.DeserializeObject<IEnumerable<GridData>>("models");
 
-            return View();
+            if (models != null)
+            {
+            }
+
+            return this.Jsonp(models);
         }
     }
 }
---
+++ LearningCurve/LearningCurve/Models/GridData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningCurve.Models
+{
+    public class GridData
+    {
+        public int ID { get; set; }
+        public string TextField { get; set; }
+        public int ForeignKeyField { get; set; }
+    }
+}
